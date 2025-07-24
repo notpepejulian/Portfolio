d8ae39f7fdb2087d64d76c4e4a7cf9189e3bb747
Nacho S. <161172067+notpepejulian@users.noreply.github.com>
2025-07-24 13:59:14 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) 5s alternate, blink .7s infinite"
+      }
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          /* Ocultar scrollbar */
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+        '.scrollbar-custom': {
+          /* Estilos personalizados para scrollbar */
+          '&::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'transparent',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(29, 80, 82, 0.5)',
+            borderRadius: '6px',
+            border: '3px solid transparent',
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#203f41',
+          },
+        },
+      });
+    },
+  ],
+}
